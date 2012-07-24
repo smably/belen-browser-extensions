f1af3b848cf8dbba4008c68e7e0d5890a7a7427f
Sylvan Mably <sylvan@gmail.com>
2012-07-24 14:51:50 -0400

    Search reset icon in Manage Ads
    Add a new icon to the right of the search button in Manage Ads, which
    resets all the search fields to their default values. Also do some
    refactoring of constants containing data: URIs.

--- extensions/belen-chrome/background.js
+++ extensions/belen-chrome/background.js
@@ -24,7 +24,13 @@ function initApplication(externalJQuery) {
 	const COLOUR_RED_HIGHLIGHT = "#FCB";
 	const COLOUR_BLUE_BACKGROUND = "#E3EDFA";
 
-	const LINK_ICON_DATA = "image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAADpSURBVCjPY/jPgB8y0EmBHXdWaeu7ef9rHuaY50jU3J33v/VdVqkdN1SBEZtP18T/L/7f/X/wf+O96kM3f9z9f+T/xP8+XUZsYAWGfsUfrr6L2Ob9J/X/pP+V/1P/e/+J2LbiYfEHQz+ICV1N3yen+3PZf977/9z/Q//X/rf/7M81Ob3pu1EXWIFuZvr7aSVBOx1/uf0PBEK3/46/gnZOK0l/r5sJVqCp6Xu99/2qt+v+T/9f+L8CSK77v+pt73vf65qaYAVqzPYGXvdTvmR/z/4ZHhfunP0p+3vKF6/79gZqzPQLSYoUAABKPQ+kpVV/igAAAABJRU5ErkJggg==";
+	const LINK_ICON_SRC = "data:image/png;base64," +
+		"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29m" +
+		"dHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAADpSURBVCjPY/jPgB8y0EmBHXdWaeu7ef9rHuaY50jU" + 
+		"3J33v/VdVqkdN1SBEZtP18T/L/7f/X/wf+O96kM3f9z9f+T/xP8+XUZsYAWGfsUfrr6L2Ob9J/X/pP+V" + 
+		"/1P/e/+J2LbiYfEHQz+ICV1N3yen+3PZf977/9z/Q//X/rf/7M81Ob3pu1EXWIFuZvr7aSVBOx1/uf0P" + 
+		"BEK3/46/gnZOK0l/r5sJVqCp6Xu99/2qt+v+T/9f+L8CSK77v+pt73vf65qaYAVqzPYGXvdTvmR/z/4Z" +
+		"HhfunP0p+3vKF6/79gZqzPQLSYoUAABKPQ+kpVV/igAAAABJRU5ErkJggg==";
 
 
 	if (location.pathname == PATH_MANAGE_ADS) {
@@ -68,7 +74,7 @@ function initApplication(externalJQuery) {
 
 			// Find all adRow tr elements that have a child element of class "meta-usrads-pstd" in which the text is precisely "1"
 			// To those matching tr elements, apply a blue background colour (these are our first-time posters)
-			$('tr.adRow').has($('span.meta-usrads-pstd').filter(function() { return $(this).text() == '1'; })).css('background-color', '#E3EDFA');
+			$('tr.adRow').has($('span.meta-usrads-pstd').filter(function() { return $(this).text() == '1'; })).css('background-color', COLOUR_BLUE_BACKGROUND);
 
 			// Function to highlight an element in red (accepts a jQuery object returns nothing)
 			var h = function(e) { e.css('padding', '1px 2px').css('background-color', COLOUR_RED_HIGHLIGHT) };
@@ -148,7 +154,7 @@ function initApplication(externalJQuery) {
 			).serialize();
 
 			if (searchParams.length > 0) {
-				$('#pg-topnav a:first-child').before('<a id="permalink"><img id="permalink_icon" src="data:' + LINK_ICON_DATA + '" /></a> ');
+				$('#pg-topnav a:first-child').before('<a id="permalink"><img id="permalink_icon" src="' + LINK_ICON_SRC + '" /></a> ');
 				$('#permalink_icon').css('border', '0').css('margin-bottom', '-4px').css('margin-right', '4px').attr('alt', 'Permalink').attr('title', 'Permanant link to this search');
 
 				$('#permalink').attr('href', PATH_MANAGE_ADS + '?formAction=submitSearch&' + searchParams);
@@ -156,7 +162,73 @@ function initApplication(externalJQuery) {
 			}
 		}
 
+		// Adds a reset icon to the right of the Search button in the top form. Clicking it resets the values in the search form to their defaults.
+		function initSearchReset() {
+
+			// This function is added to an inline script so that it is executed in the page context rather than the extension context.
+			var addResetIcon = function() {
+
+				// This can't be defined at the top of the page with the other consts.
+				// Due to the hack we are using to execute this code in the page context (needed for access to multiSelectUpdateSelected), the entire function must be self-contained.
+				const RESET_ICON_SRC = 'data:image/png;base64,' +
+					'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29m' +
+					'dHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAHsSURBVDjLtZPpTlpRFIV5Dt7AOESr1kYNThGnSomI' +
+					'ihPoNVi5Qp3RgBgvEERpRW1BRBAcMEDUtIkdjKk4otK0Jdr2vgxZ3kA0MYoaG3+cX2evb529zt4sAKz/' +
+					'OawnASgCBNm5LaE7vjVDutkA4mMdLV4TkvcCuvba2Iqd1pDhWA33mQU+2oXVv07YfpoxuNWFuqVXoeqF' +
+					'CnZcgJwRm04p+Gk3Fs9t8PyZx/K5Hfbf03CGLRj62g2+rSR0K0D+vZXUB1Xw/ou5usJWjAaU0Gz3w/rj' +
+					'Hey/ZjDLvKTD34KSyXzyBkC2JaYd4feMqyNa3OQTREQePlXjrqSq5ssj5hMjTMd66ALDKDLm0jcA0s+N' +
+					'ID6JIFmvQaNXANEKX3l5x7NyqTcb7Zg8GYtCOLoXuPcbha6XV0VlU4WUzE9gPKjF2CGFbE3G3QAmafDn' +
+					'ShETF3iKTZyIblcNza4Syi/deD6USscFCJwV6Fwn8NonQak5Hy1L9TAcjkJ/oAG1p0a1hYdnfcnkrQCB' +
+					'oxyyNYLp1YCJoB7GIwqGgxGod/oZsQoNDiHSepNCceeAN8uF1CvGxJE25rofc+3blKPqQ2VUnKxIYN85' +
+					'yty3eWh216LeKUTOSCayVGlIH0g5S+1JJB+8Cxxt1rWkH7WNTNIPAlwA9Gm7OcXUHxUAAAAASUVORK5C' +
+					'YII=';
+
+				// Reset all form values to their defaults
+				var resetSearch = function() {
+
+					// These variables are necessary for multiSelect operations
+					var messageVars = Belen.tns.controller.vars.messages;
+					var o = {
+						oneOrMoreSelected: messageVars.selectStatOneOrMoreSelected,
+						selectAllText: messageVars.selectStatSelectAll,
+						noneSelected: messageVars.selectStatNoneSelected
+					};
+
+					// Reset all the multiSelects
+					$('div.multiSelectOptions').find('INPUT:checkbox').attr('checked', false).parent().removeClass('checked');
+					$('#srch-subcat').html("<option value=''>&nbsp;</option>");
+					$('div.multiSelectOptions').multiSelectUpdateSelected(o);
+
+					// Reset the regular selects and the text inputs
+					$('#ads-srchfrm-params input.c-inpt-frm').val('');
+					$('#ads-srchfrm-params select.c-slct-frm option').removeAttr('selected');
+
+					// Restore default form values
+					// Date range = "Within the last week"
+					$('#srch-dtrng option[value="LAST_WEEK"]').attr('selected', 'true');
+					// Flags = "[no restriction]", Appeals = "[Ignore]"
+					$('#srch-flgtype option:first-child').attr('selected', 'true');
+				};
+
+				// Create reset icon from a data: url, give it a sensible title and alt text, 
+				var resetIcon = $('<img>').attr('src', RESET_ICON_SRC);
+				resetIcon.attr('alt', 'Reset search fields').attr('title', 'Reset search');
+				resetIcon.css('vertical-align', 'middle').css('margin', '-4px 2px 0 2px').css('margin-top', '-4px').css('cursor', 'pointer');
+				resetIcon.click(resetSearch);
+
+				// Add the reset icon immediately after the search button
+				$('#srch-sbmt').after(resetIcon);
+			};
+
+			// Create a script element, stuff the text of addResetIcon inside an anonymous function and immediately call it, and add all that to the end of the document body
+			var script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.appendChild(document.createTextNode('('+ addResetIcon +')();'));
+			document.body.appendChild(script);
+		}		
+
 		createPermalink();
+		initSearchReset();
 		extendKeywordField();
 		linkifyAds();
 		hlAdRisks();
